f9ca4d1b164dd031771759869b731bf6949dfdf3
DanielJensenKEA <daje0003@stud.kea.dk>
2025-03-31 14:38:58 +0200

    Side 14 af 31 SpringSecurityJWTKap2

--- src/main/java/org/example/springsecuritytutorialjwt/config/ProjectSecurityConfig.java
+++ src/main/java/org/example/springsecuritytutorialjwt/config/ProjectSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+/*
 @Configuration
 public class ProjectSecurityConfig {
     @Bean
@@ -35,4 +35,8 @@ public class ProjectSecurityConfig {
 //        return obj;
     }
 
+
+
 }
+*/
+
